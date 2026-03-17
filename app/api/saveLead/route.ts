2c3fbf8a63ef06cac24e0a94b62edcebc516a332
Rohit <freelancerrohit46@gmail.com>
2026-03-17 13:13:02 +0530

    removed consoles and ready to deploy

--- app/api/saveLead/route.ts
+++ app/api/saveLead/route.ts
@@ -6,7 +6,6 @@ export async function POST(req: Request) {
   try {
     let data = await req.json();
     data = JSON.parse(data);
-    console.log("Received lead info:", data);
     await connectDB();
     const lead = new Lead({ name: data.name, email: data.email, phone: data.phone });
     await lead.save();
